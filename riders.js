d5df6d905c5b46347ab1905294e19eaf53d59217
Slava Kim <slava@meteor.com>
2013-12-07 01:33:10 -0800

    Click on town shows summary on the bottom

--- riders.js
+++ riders.js
@@ -1,10 +1,20 @@
+Villages = new Meteor.Collection('villages');
+
 if (Meteor.isClient) {
   Template.map.rendered = function () {
     var tileUrl = 'http://a.tiles.mapbox.com/v3/mlmorg.gfnol46k/{z}/{x}/{y}.png';
-    var iconUrl = 'http://a.tiles.mapbox.com/v3/marker/pin-m-{text}+{color}.png';
+    var iconUrl = 'http://a.tiles.mapbox.com/v3/marker/pin-l-{text}+{color}.png';
 
-    function Map (el, options) {
-      this.leaflet = L.map(el);
+    function Riders () {
+      this.map = document.getElementById('map');
+      this.summary = document.getElementById('summary');
+      this.mapScreen = document.getElementById('map-screen');
+      this.detailScreen = document.getElementById('detail-screen');
+
+      this.leaflet = L.map(this.map);
+
+      this.summary.addEventListener('click', this.showDetail.bind(this));
+
       L.tileLayer(tileUrl).addTo(this.leaflet);
       this.leaflet.setView([-29.609988, 28.233608], 8);
 
@@ -14,11 +24,23 @@ if (Meteor.isClient) {
       });
     }
 
-    Map.prototype.addTown = function (latlng) {
+    Riders.prototype.addTown = function (latlng) {
       var marker = this.addMarker(latlng);
+      marker.on('click', this.showSummary.bind(this));
     };
 
-    Map.prototype.addMarker = function (latlng, options) {
+    Riders.prototype.showSummary = function () {
+      if (!this.summary.className.match(/show/)) {
+        this.summary.className += ' show';
+      }
+    };
+
+    Riders.prototype.showDetail = function () {
+      this.detailScreen.className = this.detailScreen.className.replace('out-right', '');
+      this.mapScreen.className += ' out-left';
+    };
+
+    Riders.prototype.addMarker = function (latlng, options) {
       options = options || {};
       options.icon = options.icon || this.createIcon(options);
 
@@ -28,12 +50,12 @@ if (Meteor.isClient) {
       return marker;
     };
 
-    Map.prototype.createIcon = function (options) {
+    Riders.prototype.createIcon = function (options) {
       options = L.Util.extend({
         text: 'village',
         color: '#7ec9b1',
-        iconSize: [30, 70],
-        iconAnchor: [15, 35]
+        iconSize: [35, 90],
+        iconAnchor: [18, 45]
       });
 
       options.iconUrl = options.iconUrl || L.Util.template(iconUrl, {
@@ -44,12 +66,14 @@ if (Meteor.isClient) {
       return L.icon(options);
     };
 
-    var el = document.getElementById('map');
-    new Map(el);
+    new Riders();
+
   };
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
+    if (!Villages.find().count())
+      ;
   });
 }
